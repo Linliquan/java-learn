87b7f6a37a32ae1fd36914aa36a606283d990751
Linliquan <1511387837@qq.com>
2019-04-22 00:02:38 +0800

    first commit

--- 电话号码归属地判断/src/CheckPhoneNum.java
+++ 电话号码归属地判断/src/CheckPhoneNum.java
@@ -21,17 +21,30 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ *  软件测试作业---电话号码归属地查询
+ */
 public class CheckPhoneNum extends JFrame{
-	//面板容器
+
+	/**
+	 * 面板容器
+	 */
 	private JPanel contentPane;
-	//	private JTextField nameField;
-	//电话号码
+	/**
+	 * 电话号码框
+	 */
 	private JTextField phoneField;
-//	private JTextField ageField;
 
 	public static void main(String[] args){
-
+//		CheckPhoneNum.readTxtFile("电话号码归属地判断/city.txt");
+		System.out.println(CheckPhoneNum.readTxtFile("电话号码归属地判断/city.txt"));
 		EventQueue.invokeLater(new Runnable(){
 			@Override
 			public void run(){
@@ -50,47 +63,49 @@ public class CheckPhoneNum extends JFrame{
 	public CheckPhoneNum(){
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//设置窗口位置和大小
-		setBounds(500,180,900,730);
+
+		/**
+		 * 设置窗口位置和大小
+		 */
+		setBounds(500,40,900,760);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(20,20,20,20));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel label = new JLabel("说明：输入固定的电话号码或手机号码。如13457855034，0771862435");
-		JLabel labe2 = new JLabel("<html><body><p align=\"left\">&nbsp;&nbsp;&nbsp;注意："
-				+ "<br/> &nbsp;&nbsp;&nbsp;1.必须为7~12数字</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;2.待写都是法国第三方根深蒂固商店告诉对方广东佛山高大上发给十多个</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;2.待写都是法国第三方根深蒂固商店告诉对方广东佛山高大上发给十多个</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;2.待写都是法国第三方根深蒂固商店告诉对方广东佛山高大上发给十多个</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;2.待写都是法国第三方根深蒂固商店告诉对方广东佛山高大上发给十多个</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;2.待写都是法国第三方根深蒂固商店告诉对方广东佛山高大上发给十多个</p>"
-				+ "<p>&nbsp;&nbsp;&nbsp;3.待写</p></body></html>");
+		JLabel label = new JLabel("使用说明：本系统可查询固定电话的归属地、手机号码的归属地和运营商。");
+		JLabel labe2 = new JLabel("<html><body>"
+				+ "<br/> 1.固定电话查询："
+				+ "<br/>格式：2~4位的区号+7位或8位的电话号码(有“-”分隔或者没有分隔都可以)"
+				+"<br/>如0771-86225824、0771-8622582、010-65246622、01065246622"
+				+ "<p></p>"
+				+ "<p>2.手机号码查询：</p>"
+				+ "<p>格式：输入7~11位的手机号码，如：13457855034、1345785</p>"
+				+ "<p></p>"
+				+ "<p>3.急救号码、常用号码查询：</p>"
+				+ "<p>格式：如：110、120、119、122、12315、10086、10000</p>"
+				+ "<p></p>"
+				+ "<p>注意：</p>"
+				+ "<p>1.因国内手机号码的地区号码段不外传，网上无法查询到地区号码段，" +
+				"故有一些手机号码无法查询到归属地，只能查询到运营商</p></body></html>");
 
+		JLabel labe3 = new JLabel("软件工程 软件Q164 林李权 201609601282");
+		label.setBounds(10,15,700,20);
+		labe2.setBounds(50,10,700,300);
+		labe3.setBounds(250,600,700,20);
 
-		label.setBounds(10,15,600,35);
-		labe2.setBounds(0,45,600,150);
-
+		//设置标签字体
+		label.setFont(new Font("宋体",1,16));
+		labe2.setFont(new Font("宋体",1,16));
+		labe3.setFont(new Font("宋体",1,20));
+		labe3.setForeground(Color.MAGENTA);
 		contentPane.add(label);
 		contentPane.add(labe2);
+		contentPane.add(labe3);
 
-//		nameField = new JTextField();
-//		nameField.setBounds(80,10,141,25);
-//		contentPane.add(nameField);
-
-//
-//		JLabel label_1 = new JLabel("年龄");
-//		label_1.setBounds(10,50,122,15);
-//		contentPane.add(label_1);
-//
-//		ageField = new JTextField();
-//		ageField.setBounds(80,45,141,25);
-//		contentPane.add(ageField);
-
-
-
-//		labePhone.setIcon(icon);
-
+		/**
+		 * 设置背景图片
+		 */
 		ImageIcon img = new ImageIcon("电话号码归属地判断/bg.jpg");
 		//要设置的背景图片
 		JLabel imgLabel = new JLabel(img);
@@ -100,14 +115,14 @@ public class CheckPhoneNum extends JFrame{
 		imgLabel.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
 		// 设置背景标签的位置
 		Container contain = this.getContentPane();
-		((JPanel) contain).setOpaque(false);
 		// 将内容面板设为透明。将LayeredPane面板中的背景显示出来。
-
+		((JPanel) contain).setOpaque(false);
 		validate();  //刷新
 
-		Icon icon = new ImageIcon("电话号码归属地判断/logo.png");
+		//设置手机图标
+		Icon icon = new ImageIcon("电话号码归属地判断/phone.png");
 		JLabel label_2 = new JLabel("电话号码查询");
-		label_2.setBounds(50,180,300,35);
+		label_2.setBounds(50,330,300,35);
 		contentPane.add(label_2);
 		label_2.setIcon(icon);
 
@@ -118,10 +133,10 @@ public class CheckPhoneNum extends JFrame{
 		phoneField = new JTextField();
 		phoneField.setFont(new Font("宋体",1,20));
 		phoneField.setForeground(Color.blue);
-		phoneField.setBounds(300,180,200,35);
+		phoneField.setBounds(300,330,200,35);
 		contentPane.add(phoneField);
 
-		JButton button = new JButton("验证");
+		JButton button = new JButton("查询");
 		button.setFont(new Font("宋体",1,16));
 		button.addActionListener(new ActionListener(){
 			@Override
@@ -129,15 +144,64 @@ public class CheckPhoneNum extends JFrame{
 				do_button_actionPerformed(e);
 			}
 		});
-		button.setBounds(550,180,100,35);
+		button.setBounds(550,330,100,35);
 		contentPane.add(button);
 	}
 
 	protected void do_button_actionPerformed(ActionEvent e){
 		String text = phoneField.getText();
 
-		String info = check(text);  //对输入的电话文本进行验证
+		//对输入的电话文本进行验证
+		String info = check(text);
 		JOptionPane.showMessageDialog(null,info);
+
+	}
+
+	/**
+	 *
+	 * 读取city.txt文件
+	 * @return
+	 */
+
+	/**
+	 * 功能：Java读取txt文件的内容
+	 * 步骤：1：先获得文件句柄
+	 * 2：获得文件句柄当做是输入一个字节码流，需要对这个输入流进行读取
+	 * 3：读取到输入流后，需要读取生成字节流
+	 * 4：一行一行的输出。readline()。
+	 * 备注：需要考虑的是异常情况
+	 * @param filePath
+	 */
+
+
+	public static Map<String, String> map = new HashMap<>();
+
+	public static Map<String, String> readTxtFile(String filePath){
+
+		try {
+			String encoding="GBK";
+			File file=new File(filePath);
+			if(file.isFile() && file.exists()){ //判断文件是否存在
+				InputStreamReader read = new InputStreamReader(
+						new FileInputStream(file),encoding);//考虑到编码格式,要同IDE的编码一样
+				BufferedReader bufferedReader = new BufferedReader(read);
+				String lineTxt = null;
+				while((lineTxt = bufferedReader.readLine()) != null){
+                        System.out.println(lineTxt);
+					String[] split = lineTxt.split("\\s+");
+					map.put(split[0],split[1]);
+				}
+				read.close();
+//				System.out.println(map.get("21"));
+
+			}else{
+				System.out.println("找不到指定的文件");
+			}
+		} catch (Exception e) {
+//            System.out.println("读取文件内容出错");
+//            e.printStackTrace();
+		}
+		return map;
 	}
 
 	public String check(String text){
@@ -145,141 +209,93 @@ public class CheckPhoneNum extends JFrame{
 			return "请输入电话号码";
 		}
 
-
-		//常用急救号码、短号
+		/**
+		 *  急救号码
+		 */
 		if(text.equals("110")) {
+			System.out.println("电话号码："+text+" 报警电话");
 			return text + "\n报警电话";
 		}
 		if(text.equals("120")) {
+			System.out.println("电话号码："+text+" 报急救中心");
 			return text + "\n急救中心";
 		}
 		if(text.equals("119")) {
+			System.out.println("电话号码："+text+" 火警");
 			return text + "\n火警";
 		}
 		if(text.equals("122")) {
+			System.out.println("电话号码："+text+" 道路交通事故报警");
 			return text + "\n道路交通事故报警";
 		}
 		if(text.equals("112")) {
+			System.out.println("电话号码："+text+" 通用紧急求救");
 			return text + "\n通用紧急求救";
 		}
 		if(text.equals("10086")) {
+			System.out.println("电话号码："+text+" 中国移动");
 			return text + "\n中国移动";
 		}
 		if(text.equals("10010")) {
+			System.out.println("电话号码："+text+" 中国联通");
 			return text + "\n中国联通";
 		}
 		if(text.equals("10000")) {
+			System.out.println("电话号码："+text+" 中国电信");
 			return text + "\n中国电信";
 		}
 		if(text.equals("12315")) {
+			System.out.println("电话号码："+text+" 消费者申诉举报");
 			return text + "\n消费者申诉举报";
 		}
 
 
-		//固定电话匹配，如0771-86245687、077186245687、771-86245687、77186245687、010-52365215、13457852366、1345785
-		//7~12位数字可通过此正则表达式
-		String regex = "^\\d{7,11}|\\d{2}-?\\d{7}|\\d{2}-?\\d{8}|\\d{3}-?\\d{7}|\\d{3}-?\\d{8}|\\d{4}-?\\d{8}|\\d{4}-?\\d{7}$";  //正则表达式
+
+		/**
+		 * 	固定电话匹配，如0771-86245687、077186245687、771-86245687、77186245687、010-52365215、13457852366、1345785
+		 * 	7~12位数字可通过此正则表达式
+		 * 	正则表达式
+		 */
+		String regex = "^\\d{7,11}|\\d{2}-?\\d{7}|\\d{2}-?\\d{8}|\\d{3}-?\\d{7}|\\d{3}-?\\d{8}|\\d{4}-?\\d{8}|\\d{4}-?\\d{7}$";
 
 		if(text.matches(regex)){
 
-			/****************************************************************************************
-			 ************************   固定电话号码归属地查询        *******************************************
-			 ****************************************************************************************/
 
-			//广西
-			if((text.substring(0, 4)).equals("0771")||(text.substring(0, 3)).equals("771")) {
-				return text + "\n归属地：广西    南宁";
-			}
-			if((text.substring(0, 4)).equals("0776")||(text.substring(0, 3)).equals("776")) {
-				return text + "\n归属地：广西    百色";
-			}
-			if((text.substring(0, 4)).equals("0779")||(text.substring(0, 3)).equals("779")) {
-				return text + "\n归属地：广西    北海";
-			}
-			if((text.substring(0, 4)).equals("0773")||(text.substring(0, 3)).equals("773")) {
-				return text + "\n归属地：广西    桂林";
-			}
-			if((text.substring(0, 4)).equals("0772")||(text.substring(0, 3)).equals("772")) {
-				return text + "\n归属地：广西    柳州";
-			}
-			if((text.substring(0, 4)).equals("0778")||(text.substring(0, 3)).equals("778")) {
-				return text + "\n归属地：广西    河池";
+			//对key循环,4位
+			for(String key : map.keySet()) {
+				String str =text.substring(0,4);
+				if((map.get(key)).equals(str)) {
+					System.out.println("电话号码："+text+"  归属地："+key);
+					return text + "\n归属地："+key;
+				}
 			}
 
-
-			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-				return text + "\n归属地：北京";
+			//对key循环，3位
+			for(String key : map.keySet()) {
+				String str =text.substring(0,3);
+				if((map.get(key)).equals(str)) {
+					System.out.println("电话号码："+text+"  归属地："+key);
+					return text + "\n归属地："+key;
+				}
 			}
-			if((text.substring(0, 3)).equals("021")||(text.substring(0, 2)).equals("21")) {
-				return text + "\n归属地：上海";
-			}
-			if((text.substring(0, 3)).equals("022")||(text.substring(0, 2)).equals("22")) {
-				return text + "\n归属地：天津";
-			}
-			if((text.substring(0, 3)).equals("023")||(text.substring(0, 2)).equals("23")) {
-				return text + "\n归属地：重庆";
-			}
-
-			//内蒙古
-			if((text.substring(0, 4)).equals("0471")||(text.substring(0, 3)).equals("471")) {
-				return text + "\n归属地：内蒙古   呼和浩特市";
-			}
-			//待添加。。。
-
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
-//			if((text.substring(0, 3)).equals("010")||(text.substring(0, 2)).equals("10")) {
-//				return text + "\n归属地：北京";
-//			}
 
 			/****************************************************************************************
-			 ******************** 以下是牛逼的手机号码归属地判断 **************************************
+			 ******************** 以下是牛逼的手机号码归属地判断 ************************************
 			 ****************************************************************************************/
 
-			//移动号码归属地
-			//依据前面3位判断运营商，4~7位判断手机号的地区
 
+			/**
+			 * 移动号码归属地
+			 * 	依据前面3位判断运营商，4~7位判断手机号的地区
+			 */
 			String regexCm = "^1(3[4-9]|4[7]|5[0|1|2|7|8|9]|7[8]|8[2|3|4|7|8])\\d{4,8}$";
 			if(text.matches(regexCm)){
 				if((text.substring(3, 7)).equals("5785")) {
+					System.out.println("电话号码："+text+"  归属地：广西 南宁(移动)");
 					return text + "\n归属地：广西   南宁 (移动)";
 				}
 				//待补充
-
+				System.out.println("电话号码："+text+"  移动");
 				return text + "\n移动";
 			}
 
@@ -288,7 +304,7 @@ public class CheckPhoneNum extends JFrame{
 			if(text.matches(regexCu)){
 
 				//待补充
-
+				System.out.println("电话号码："+text+"  联通");
 				return text + "\n联通";
 			}
 
@@ -297,10 +313,9 @@ public class CheckPhoneNum extends JFrame{
 			if(text.matches(regexCt)){
 
 				//待补充
-
+				System.out.println("电话号码："+text+"  电信");
 				return text + "\n电信";
 			}
-
 
 			return text + "\n请输入正确的电话号码！";
 		}else{
