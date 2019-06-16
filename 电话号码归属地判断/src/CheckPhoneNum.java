973e45505fd4f723316c53265edca7166ea44714
Linliquan <1511387837@qq.com>
2019-06-16 19:06:44 +0800

    first commit

--- 电话号码归属地判断/src/CheckPhoneNum.java
+++ 电话号码归属地判断/src/CheckPhoneNum.java
@@ -383,12 +383,6 @@ public class CheckPhoneNum extends JFrame {
 //          System.out.println(province);
 //          System.out.println(province);
 //          System.out.println(province);
-//          System.out.println(province);
-//          System.out.println(province);
-//          System.out.println(province);
-//          System.out.println(province);
-//          System.out.println(province);
-//          System.out.println(province);
 
 
 
