13caf82ba7a5e9442512b9e3ede8765b201875cd
Linliquan <1511387837@qq.com>
2019-08-11 23:33:32 +0800

    first commit

---
+++ 反射/src/Student.java
@@ -0,0 +1,43 @@
+public class Student {
+    private int id;
+    private String name;
+
+    public Student() {
+    }
+
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    //反射测试
+    public String show(int num){
+        System.out.println("num="+num);
+        return "hello";
+    }
+}
---
+++ 反射/src/Test.java
@@ -0,0 +1,45 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * 反射练习
+         */
+        //1.获得Class引用
+        Class<Student> s = Student.class;
+
+        //2.获得Student的属性
+        //获得非私有属性
+        Field [] f = s.getFields();
+
+        //获得私有属性
+        Field [] fields = s.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+           Field field = fields[i];
+            System.out.println(field.getName());
+        }
+
+        //3.获得所有的方法
+        Method [] methods = s.getMethods();
+        for (int i = 0; i <methods.length ; i++) {
+            Method method = methods[i];
+            System.out.println(method.getName());
+        }
+
+        //
+      Method method =  s.getMethod("show",int.class);
+
+        //反射创建对象
+        Student student = s.newInstance();
+        Object invole = method.invoke(student,30);
+        System.out.println(invole);
+
+
+
+
+    }
+}
