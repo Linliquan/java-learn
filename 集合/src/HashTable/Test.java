1bb2daef80b85022246b10d74c8b3496835ba358
Linliquan <1511387837@qq.com>
2019-08-17 23:11:56 +0800

    first commit

---
+++ 集合/src/HashTable/Test.java
@@ -0,0 +1,21 @@
+package HashTable;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(3,"fdg");
+        hashtable.put(3,"fdg");
+        hashtable.put(6,"jj");
+        hashtable.put(2,"kk");
+
+        for (Map.Entry<Integer,String> entry: hashtable.entrySet()){
+            System.out.println(entry.getKey()+ " " + entry.getValue());
+        }
+
+
+
+    }
+}
