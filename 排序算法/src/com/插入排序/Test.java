c3e1d0b333c1b4a9c6051c63853d10c278b7d207
Linliquan <1511387837@qq.com>
2019-08-28 14:25:03 +0800

    insert

---
+++ 排序算法/src/com/插入排序/Test.java
@@ -0,0 +1,24 @@
+package com.插入排序;
+
+/*
+public class Test {
+    public static void main(String[] args) {
+        int [] a = {6,2,3,1,8,5};
+
+        for (int i = 1; i < a.length; i++) {
+
+            int j = i;
+            int temp = a[i];
+            for ( ; j > 0 && temp < a[j-1]; j--) {
+                a[j] = a[j-1];
+            }
+            a[j] = temp;
+        }
+
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+    }
+}
+
+*/
