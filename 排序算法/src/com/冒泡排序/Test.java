93aab90543353b290f5b9d3c2079b16c6aa35014
Linliquan <1511387837@qq.com>
2019-08-28 14:24:25 +0800

    dubble

---
+++ 排序算法/src/com/冒泡排序/Test.java
@@ -0,0 +1,23 @@
+package com.冒泡排序;
+/*
+public class Test {
+    public static void main(String[] args){
+        int [] a = {3,6,1,4,3,8,5};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length-1-i; j++) {
+                if(a[j] > a[j+1]){
+                    int t = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = t;
+                }
+            }
+        }
+
+        for(int i : a){
+            System.out.print(i+" ");
+        }
+    }
+}
+
+*/
