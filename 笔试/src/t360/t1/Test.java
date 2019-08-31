5432151b9289a6c02966ec9ccb91a968c723023d
Linliquan <1511387837@qq.com>
2019-08-31 23:14:53 +0800

    first commit

---
+++ 笔试/src/t360/t1/Test.java
@@ -0,0 +1,19 @@
+package t360.t1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [] a = new int[m];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println(8);
+
+    }
+}
