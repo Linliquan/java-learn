517234b475826e0d48b4c26790a260bb807b3779
Linliquan <1511387837@qq.com>
2019-09-06 23:17:07 +0800

    first commit

---
+++ 笔试/src/juren/t1/Test.java
@@ -0,0 +1,20 @@
+package juren.t1;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
