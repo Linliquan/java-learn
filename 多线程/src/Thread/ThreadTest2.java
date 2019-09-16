33414bd3203b9241e01d3d80de385acfe8f4515b
Linliquan <1511387837@qq.com>
2019-09-16 20:29:25 +0800

    first commit

--- 多线程/src/Thread/ThreadTest2.java
+++ 多线程/src/Thread/ThreadTest2.java
@@ -1,19 +1,29 @@
 package Thread;
 
 public class ThreadTest2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         My my1 = new My();
         My my2 = new My();
         my1.start();
+        my1.join();
         my2.start();
+
     }
 }
 
 class My extends Thread{
     @Override
     public void run() {
+
+
         for (int i = 0; i < 20; i++) {
             System.out.println(Thread.currentThread().getName()+" "+i);
+
+//            //t1、t2线程交替打印
+//            try{
+//                Thread.sleep(100);
+//            }catch(Exception e){
+//            }
         }
     }
 }
