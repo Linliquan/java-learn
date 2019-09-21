2f0091a01bd9e2e78304043c7ebdc13da3f0019b
Linliquan <1511387837@qq.com>
2019-09-21 23:06:06 +0800

    first commit

---
+++ 笔试/src/shangtang/t1/Test.java
@@ -0,0 +1,10 @@
+package shangtang.t1;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c =10;
+        System.out.println(""+a+b+c);
+    }
+}
