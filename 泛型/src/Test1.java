522075399b117487fd7918b8c6fbaa7f9cf599f5
Linliquan <1511387837@qq.com>
2019-09-21 23:18:53 +0800

    first commit

---
+++ 泛型/src/Test1.java
@@ -0,0 +1,18 @@
+public class Test1 <T>{
+
+    public void insert(T t){
+        System.out.println("插入成功");
+    }
+
+    public void del(T t){
+        System.out.println("删除成功");
+    }
+
+    public void redi(T t){
+        System.out.println("修改成功");
+    }
+
+    public void find(T t){
+        System.out.println("查找成功");
+    }
+}
