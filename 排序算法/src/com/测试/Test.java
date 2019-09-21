5ba5e118d9f677ce1da217ab2fbc4448c2803add
Linliquan <1511387837@qq.com>
2019-09-21 23:16:34 +0800

    first commit

--- 排序算法/src/com/测试/Test.java
+++ 排序算法/src/com/测试/Test.java
@@ -2,17 +2,51 @@ package com.测试;
 
 public class Test {
     public static void main(String[] args) {
-        int a = 5;
-        String str = "101";
+        int [] a = {3,1,6,8,7,4,5};
 
-        //int类型转String类型
-        String s = String.valueOf(5);
-        s.equals("4");
-        System.out.println(s);
+//        //冒泡排序
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = 0; j < a.length-1-i; j++) {
+//                if(a[j] > a[j+1]){
+//                    int t = a[j];
+//                    a[j] = a[j+1];
+//                    a[j+1] = t;
+//                }
+//            }
+//        }
 
-        //String类型转int类型
-        int b  = Integer.parseInt(str);
-        System.out.println(b);
+
+//        //选择排序
+//        for (int i = 0; i < a.length; i++) {
+//
+//            int mix = i;
+//
+//            for (int j = i+1; j < a.length; j++) {
+//              if(a[j] < a[mix]){
+//                  mix = j;
+//              }
+//            }
+//            int t = a[i];
+//            a[i] = a[mix];
+//            a[mix] = t;
+//
+//        }
+
+//        //插入排序
+//        for (int i = 1; i < a.length; i++) {
+//
+//            int j = i;
+//            int temp = a[i];
+//
+//            for (; j > 0 && a[j-1] > temp; j--) {
+//                    a[j] = a[j-1];
+//            }
+//            a[j] = temp;
+//        }
+
+        for(int i : a){
+            System.out.print(i+ " ");
+        }
 
     }
 }
