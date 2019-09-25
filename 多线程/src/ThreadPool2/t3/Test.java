447f629b4f5f866872e5682ecfb571e888bb81d6
Linliquan <1511387837@qq.com>
2019-09-25 19:40:11 +0800

    first commit

---
+++ 多线程/src/ThreadPool2/t3/Test.java
@@ -0,0 +1,24 @@
+package ThreadPool2.t3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        MyThreadPool myThreadPool = new MyThreadPool();
+
+        for (int i = 0; i < 5; i++) {
+            service.submit(myThreadPool);
+        }
+        service.shutdown();
+    }
+}
+class MyThreadPool implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+}
