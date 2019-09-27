6e79e2e4a8e97c7fce908997c50ece8024a4e538
Linliquan <1511387837@qq.com>
2019-09-27 16:39:02 +0800

    first commit

---
+++ IO流/src/遍历文件/t1/Test.java
@@ -0,0 +1,28 @@
+package 遍历文件.t1;
+
+
+import java.io.File;
+
+//遍历文件以及子文件
+public class Test {
+
+    public static void showDirectory(File file){
+        File [] a = file.listFiles();
+
+        try {
+            for(File b : a){
+                System.out.println(b);
+                if(file.isDirectory()){
+                    showDirectory(b);
+                }
+            }
+        } catch (Exception e) {
+        }
+
+    }
+
+    public static void main(String[] args) {
+        File file = new File("F:\\城市");
+        showDirectory(file);
+    }
+}
