348480b783c12c8cb853776b7c91f8cd593d3f66
Linliquan <1511387837@qq.com>
2019-09-28 10:51:49 +0800

    first commit

--- 多线程/src/ThreadJoin/Test.java
+++ 多线程/src/ThreadJoin/Test.java
@@ -1,12 +1,12 @@
 package ThreadJoin;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         MyThread1 myThread1 = new MyThread1();
         MyThread2 myThread2 = new MyThread2();
         myThread1.start();
-
+        myThread1.join();
         myThread2.start();
 
 
@@ -21,10 +21,6 @@ class MyThread1 extends Thread{
     @Override
     public void run(){
         for (int i = 1; i <= 20; i++) {
-
-            if (i == 8){
-
-            }
             System.out.println(Thread.currentThread().getName()+" "+i);
         }
     }
