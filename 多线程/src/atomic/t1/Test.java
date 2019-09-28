eb3a0685e1099cebfa4d2e459ab76dbb97bf85d6
Linliquan <1511387837@qq.com>
2019-09-28 11:00:34 +0800

    first commit

---
+++ 多线程/src/atomic/t1/Test.java
@@ -0,0 +1,28 @@
+package atomic.t1;
+
+public class Test {
+    static int  a = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread [] threads = new Thread[2];
+        for (int i = 0; i < threads.length; i++) {
+            threads [i] = new Thread(){
+                @Override
+                public void run() {
+                    for (int j = 0; j < 10000; j++) {
+                        a++;
+                    }
+                }
+            };
+            threads[i].start();
+        }
+        for (Thread thread : threads){
+           thread.join();
+        }
+        System.out.println(a);
+    }
+
+
+}
+
+
