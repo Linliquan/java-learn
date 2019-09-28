097b1ddaf607b4fb00debb2752afa9f7ebd9d902
Linliquan <1511387837@qq.com>
2019-09-28 11:00:57 +0800

    first commit

---
+++ 多线程/src/atomic/t2/Test.java
@@ -0,0 +1,39 @@
+package atomic.t2;
+
+
+public class Test {
+   public static int  a = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new MyThread1());
+        Thread t2 = new Thread(new MyThread2());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(a);
+    }
+}
+class MyThread1 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100000; i++) {
+            Test.a++;
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+}
+
+class MyThread2 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100000; i++) {
+            Test.a++;
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
+    }
+}
+
+
