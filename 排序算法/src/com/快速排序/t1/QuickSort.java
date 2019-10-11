60c9ca898cba4264a0bd91b513a985c071c8dff7
Linliquan <1511387837@qq.com>
2019-10-11 14:20:11 +0800

    first commit

--- 排序算法/src/com/快速排序/t1/QuickSort.java
+++ 排序算法/src/com/快速排序/t1/QuickSort.java
@@ -1,15 +1,15 @@
-package com.快速排序;
+package com.快速排序.t1;
 
 import java.util.Arrays;
 
 public class QuickSort {
 
-    public static void quiackSort(int [] arr,int left,int right){
+    public static void quickSort(int [] arr,int left,int right){
         int pivot = 0;
         if(left < right){
             pivot = partition(arr,left,right);
-            quiackSort(arr,left,pivot-1);
-            quiackSort(arr,pivot+1,right);
+            quickSort(arr,left,pivot-1);
+            quickSort(arr,pivot+1,right);
         }
     }
 
@@ -32,9 +32,10 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-        int [] arr= {65,58,95,10,57,62,13,106,78,23,85};
-        System.out.println(Arrays.toString(arr));
-        quiackSort(arr,0,args.length-1);
-        System.out.println(Arrays.toString(arr));
+        int arr[] = {65, 58, 95, 10, 57, 62, 13, 106, 78, 23, 85};
+        System.out.println("未排序："+Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("排序后："+Arrays.toString(arr));
+
     }
 }
