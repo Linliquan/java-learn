3130e7448f366065c58851794661b850161e8690
Linliquan <1511387837@qq.com>
2019-10-13 09:49:01 +0800

    first commit

---
+++ Java新特性/src/函数式接口3/O10.java
@@ -0,0 +1,25 @@
+package 函数式接口3;
+
+import 函数式编程2.IT8;
+
+public class O10 {
+    public void todo(IT10 t){
+        System.out.println("重要操作1");
+        System.out.println("重要操作2");
+        String show = t.show(10);
+
+        System.out.println("返回值："+show);
+      //  t.show(10);
+    }
+
+    public static void main(String[] args) {
+        O10 o = new O10();
+
+//        IT10 t1 = a -> "tom";
+//        o.todo(t1);
+
+        o.todo(a -> "tom");
+
+
+    }
+}
