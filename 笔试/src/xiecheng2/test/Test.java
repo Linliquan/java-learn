962466bcb2dab7a88dee38491ae79080bfaabda5
Linliquan <1511387837@qq.com>
2019-10-15 09:25:18 +0800

    first commit

---
+++ 笔试/src/xiecheng2/test/Test.java
@@ -0,0 +1,22 @@
+package xiecheng2.test;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String args[]){
+        int m;
+        double sum,n;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            n=sc.nextInt();
+            m=sc.nextInt();
+            sum=0;
+            for(int i=0;i<m;i++){
+                sum=sum+n;
+                n=Math.sqrt(n);
+            }
+            System.out.printf("%.2f",sum);
+            System.out.println();
+        }
+    }
+}
