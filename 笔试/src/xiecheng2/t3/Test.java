d4c6bc5da392faff32f267f102a8e4f7ce9991ee
Linliquan <1511387837@qq.com>
2019-10-15 09:24:53 +0800

    first commit

---
+++ 笔试/src/xiecheng2/t3/Test.java
@@ -0,0 +1,16 @@
+package xiecheng2.t3;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println(8);
+    }
+}
