65e96e151c27e67c11e1ce7d9cb7b06da12b871c
Linliquan <1511387837@qq.com>
2019-11-05 20:14:53 +0800

    first commit

---
+++ 笔试/src/wangwei/t3/Test.java
@@ -0,0 +1,25 @@
+package wangwei.t3;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int [][] a = new int[6][6];
+        int [] b = new int[5];
+        for(int i = 0; i < 6; i++){
+            for (int j = 0; j < 6; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 6; i++){
+            for (int j = 0; j < 6; j++) {
+
+            }
+            System.out.println();
+        }
+
+    }
+}
