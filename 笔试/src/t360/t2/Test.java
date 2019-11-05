6316f777cd0e8e4e26940d35284a5927b9f9359d
Linliquan <1511387837@qq.com>
2019-11-05 20:19:51 +0800

    first commit

---
+++ 笔试/src/t360/t2/Test.java
@@ -0,0 +1,36 @@
+//package t360.t2;
+//
+//import java.io.*;
+//import java.util.*;
+//import java.text.*;
+//import java.math.*;
+//import java.util.regex.*;
+//
+//public class Test {
+//
+//
+//    /*请完成下面这个函数，实现题目要求的功能
+//    当然，你也可以不按照下面这个模板来作答，完全按照自己的想法来 ^-^
+//    ******************************开始写代码******************************/
+//    static String resolve(String expr) {
+//
+//
+//    }
+//    /******************************结束写代码******************************/
+//
+//
+//    public static void main(String[] args){
+//        Scanner in = new Scanner(System.in);
+//        String res;
+//
+//        String _expr;
+//        try {
+//            _expr = in.nextLine();
+//        } catch (Exception e) {
+//            _expr = null;
+//        }
+//
+//        res = resolve(_expr);
+//        System.out.println(res);
+//    }
+//}
