7d5c8e830b170a4a886ac3069d25e2ddc30ad05e
Linliquan <1511387837@qq.com>
2019-11-08 17:06:48 +0800

    first commit

---
+++ springboot-config/src/main/java/com/spring/springbootconfig/bean/Group.java
@@ -0,0 +1,26 @@
+package com.spring.springbootconfig.bean;
+
+public class Group {
+    private int id;
+    private String  name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
