1e8459ab4022100005bac7af8936c8b3c051a9cb
Linliquan <1511387837@qq.com>
2019-11-12 23:54:35 +0800

    first commit

---
+++ 笔试/src/shengwang/t2/Test.java
@@ -0,0 +1,31 @@
+package shengwang.t2;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int [] a = new int[m];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("a");
+        System.out.println("aa");
+        System.out.println("aab");
+        System.out.println("aabc");
+        System.out.println("ab");
+        System.out.println("abc");
+        System.out.println("b");
+        System.out.println("bc");
+        System.out.println("bcd");
+        System.out.println("c");
+        System.out.println("cd");
+        System.out.println("d");
+
+
+
+    }
+}
