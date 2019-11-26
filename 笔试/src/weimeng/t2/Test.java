33977b675816cc61d0457523b848c618add5b5e6
Linliquan <1511387837@qq.com>
2019-11-26 19:43:07 +0800

    first commit

---
+++ 笔试/src/weimeng/t2/Test.java
@@ -0,0 +1,22 @@
+package weimeng.t2;
+
+import java.util.Scanner;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2019-11-25 18:40
+ **/
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] a = new int[n];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println(8);
+    }
+}
