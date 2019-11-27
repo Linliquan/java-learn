be08f734f062daa72b754474d6b2d3d275b65f6b
Linliquan <1511387837@qq.com>
2019-11-27 19:08:49 +0800

    first commit

---
+++ cms_jd1908/src/main/java/com/briup/apps/cms/vm/UserVM.java
@@ -0,0 +1,29 @@
+package com.briup.apps.cms.vm;
+
+/**
+ * @program: cms_jd1911
+ * @description: 用户viewmodel类
+ * @author: charles
+ * @create: 2019-11-15 14:49
+ **/
+
+public class UserVM {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
