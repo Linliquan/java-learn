04f8731ad6be81e5f584ff8cbd23aa5cd312a472
linliquan <1511387837@qq.com>
2020-03-27 12:35:54 +0800

    first commit

---
+++ JavaTest/src/test/Test.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-03-07 15:25
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+        Integer a = 1;
+        String b = "0";
+
+        if (1 != 2){
+            System.out.println("阿斯蒂芬");
+        }else {
+            System.out.println("哈哈");
+        }
+
+
+
+
+
+    }
+}
