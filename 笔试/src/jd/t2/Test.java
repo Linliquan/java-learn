71eb8a874b65593e4f6b2e3afc3d4e2d1ca8c15f
linliquan <1511387837@qq.com>
2020-03-30 16:27:09 +0800

    first commit

---
+++ 笔试/src/jd/t2/Test.java
@@ -0,0 +1,21 @@
+package jd.t2;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int [] a = new int[n*m];
+
+        for (int i = 0; i < n*m; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println(1);
+        System.out.println(1);
+    }
+}
