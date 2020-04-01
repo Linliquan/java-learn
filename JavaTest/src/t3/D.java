a3bb76246dc0927f9e0959e76da111692fba2549
linliquan <1511387837@qq.com>
2020-04-01 13:14:26 +0800

    first commit

---
+++ JavaTest/src/t3/D.java
@@ -0,0 +1,13 @@
+package t3;
+
+import t1.A;
+
+public class D {
+    public static void main(String[] args) {
+
+        A a = new A();
+        System.out.println(a.age);
+        System.out.println(a);
+
+    }
+}
