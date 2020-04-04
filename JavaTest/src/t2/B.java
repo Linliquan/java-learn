49f650122e2c0064c195ca85cf196e949cf99280
linliquan <1511387837@qq.com>
2020-04-04 11:52:08 +0800

    first commit

---
+++ JavaTest/src/t2/B.java
@@ -0,0 +1,12 @@
+package t2;
+
+import t1.A;
+
+public class B extends A {
+    public static void main(String[] args) {
+        B a = new B();
+      System.out.println(a.age);
+      a.f();
+    }
+
+}
