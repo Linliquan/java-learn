49bbf1f53fc73bb36a4fe69f7aaecbfa3fa8d594
linliquan <1511387837@qq.com>
2020-04-12 17:16:30 +0800

    first commit

--- JavaTest/src/test/Test.java
+++ JavaTest/src/test/Test.java
@@ -1,9 +1,6 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @program: java-learn
@@ -14,6 +11,7 @@ import java.util.Map;
 public class Test {
     public static void main(String[] args) {
 
+        Date date3 = new Date();
         Integer a = 1;
         String b = "0";
 
@@ -21,6 +19,32 @@ public class Test {
             System.out.println("阿斯蒂芬");
         }else {
             System.out.println("哈哈 github github");
+        }
+
+        List<Integer> staffIds = new ArrayList<>();
+        staffIds.add(234);
+        staffIds.add(1);
+        staffIds.add(34);
+        staffIds.add(67);
+        staffIds.add(3);
+        staffIds.add(99);
+
+        Random random = new Random();
+        int n = random.nextInt(staffIds.size());
+        System.out.println( staffIds.get(n));
+
+        long hostitalIntervalTime = (100000 / (1000 * 60));
+        System.out.println((int)hostitalIntervalTime);
+
+         Map<String,String> hashMap = new HashMap<>();
+         hashMap.put("a",null);
+         hashMap.put("b",null);
+         hashMap.put("",null);
+         hashMap.put(null,null);
+
+        System.out.println("哈哈");
+        for (String key: hashMap.keySet()) {
+            System.out.println(key + " "+ hashMap.get(key));
         }
 
 
