e596ad561d5106b917252f0dda02ea948d9836aa
linliquan <1511387837@qq.com>
2020-05-14 08:16:46 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * @author zhouli
+ * @author linliquan
  */
 @SpringBootApplication
 public class ConsumerMovieApplication {
