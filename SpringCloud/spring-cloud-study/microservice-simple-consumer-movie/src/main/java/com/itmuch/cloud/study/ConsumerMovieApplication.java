038c87321012d24679204218da3808a5308e7f8c
linliquan <1511387837@qq.com>
2020-05-15 12:51:11 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -10,12 +10,12 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 public class ConsumerMovieApplication {
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-  public static void main(String[] args) {
-    SpringApplication.run(ConsumerMovieApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
 }
