12f4379d6c1644d118a6ea925ed6b0b1362bd78a
linliquan <1511387837@qq.com>
2020-05-20 15:43:50 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -2,12 +2,14 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author linliquan
  */
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ConsumerMovieApplication {
     @Bean
