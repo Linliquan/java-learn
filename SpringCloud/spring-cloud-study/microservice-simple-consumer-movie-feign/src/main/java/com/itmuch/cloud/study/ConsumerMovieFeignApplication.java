6505897e14aec09b2387e42a96b6ce8911314c3a
linliquan <1511387837@qq.com>
2020-05-20 15:48:39 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/ConsumerMovieFeignApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/ConsumerMovieFeignApplication.java
@@ -2,12 +2,14 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author linliquan
  */
 @EnableFeignClients
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ConsumerMovieFeignApplication {
     public static void main(String[] args) {
