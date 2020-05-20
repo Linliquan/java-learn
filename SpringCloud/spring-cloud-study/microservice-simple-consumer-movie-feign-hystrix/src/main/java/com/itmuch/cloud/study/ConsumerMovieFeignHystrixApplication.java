fc4ee6de915450a01e996f6311434f9daeb73bfc
linliquan <1511387837@qq.com>
2020-05-20 16:03:21 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-consumer-movie-feign-hystrix/src/main/java/com/itmuch/cloud/study/ConsumerMovieFeignHystrixApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-consumer-movie-feign-hystrix/src/main/java/com/itmuch/cloud/study/ConsumerMovieFeignHystrixApplication.java
@@ -2,12 +2,16 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author linliquan
  */
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class ConsumerMovieFeignHystrixApplication {
     public static void main(String[] args) {
