8afbb4562117f48f90db3e8f886ffe727f6104f6
linliquan <1511387837@qq.com>
2020-05-24 12:01:46 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-simple-provider-user-sleuth/src/main/java/com/itmuch/cloud/study/ProviderUserSleuthApplication.java
+++ SpringCloud/spring-cloud-study/microservice-simple-provider-user-sleuth/src/main/java/com/itmuch/cloud/study/ProviderUserSleuthApplication.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 /**
- * @author zhouli
+ * @author linliquan
  */
 @SpringBootApplication
 public class ProviderUserSleuthApplication {
