13c7146c784daa9e12e5c0cf62b18e1b1908ae17
linliquan <1511387837@qq.com>
2020-05-26 23:10:58 +0800

    first commit

--- SpringCloudAlibaba/microservice-simple-provider-user-ali/src/main/java/com/itmuch/cloud/study/ProviderUserAliApplication.java
+++ SpringCloudAlibaba/microservice-simple-provider-user-ali/src/main/java/com/itmuch/cloud/study/ProviderUserAliApplication.java
@@ -5,6 +5,7 @@ import com.itmuch.cloud.study.repository.UserRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.util.stream.Stream;
 /**
  * @author zhouli
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserAliApplication {
     public static void main(String[] args) {
