ed778b4d6120e3c09649e083ff5d8b52d058ebd9
linliquan <1511387837@qq.com>
2020-05-29 21:07:01 +0800

    first commit

---
+++ SpringCloud/spring-cloud-study/microservice-discovery-eureka-authenticating/src/main/java/com/springcloud/server/discovery/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.springcloud.server.discovery.config;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-05-24 16:05
+ **/
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Cloud Finchley及更高版本，必须添加如下代码，部分关闭掉Spring Security
+ * 的CSRF保护功能，否则应用无法正常注册！
+ * ref: http://cloud.spring.io/spring-cloud-netflix/single/spring-cloud-netflix.html#_securing_the_eureka_server
+ * @author zhouli
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
