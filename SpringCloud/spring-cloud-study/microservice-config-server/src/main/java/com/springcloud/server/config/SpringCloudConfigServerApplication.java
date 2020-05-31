baebf8e13737fb211c8c12c798cd18b3b113b57b
linliquan <1511387837@qq.com>
2020-06-01 00:27:03 +0800

    first commit

--- SpringCloud/spring-cloud-study/microservice-config-server/src/main/java/com/springcloud/server/config/SpringCloudConfigServerApplication.java
+++ SpringCloud/spring-cloud-study/microservice-config-server/src/main/java/com/springcloud/server/config/SpringCloudConfigServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @program: java-learn
- * @description: Zuul是Netflix开源的微服务网关,微服务网关是介于客户端和服务器端之间的中间层，
- * 所有的外部请求都会先经过微服务网关,减少了客户端与各个微服务之间的交互次数。
+ * @description:
  * @author: liquan
  * @create: 2020-05-14 18:23
  **/
