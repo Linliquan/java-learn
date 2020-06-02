9006733adedb67b3358db2ce1f04d37db215bbbe
linliquan <1511387837@qq.com>
2020-06-02 23:13:16 +0800

    first commit

--- springcloudconfig/microservice-config-client-2/src/main/java/com/itmuch/cloud/study/ConfigClient2Application.java
+++ springcloudconfig/microservice-config-client-2/src/main/java/com/itmuch/cloud/study/ConfigClient2Application.java
@@ -2,10 +2,12 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigClientApplication {
+public class ConfigClient2Application {
   public static void main(String[] args) {
-    SpringApplication.run(ConfigClientApplication.class, args);
+    SpringApplication.run(ConfigClient2Application.class, args);
   }
 }
