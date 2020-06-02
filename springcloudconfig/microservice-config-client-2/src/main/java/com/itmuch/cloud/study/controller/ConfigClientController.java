7955c7709ba9d3fad342674c989993886eb87ec7
linliquan <1511387837@qq.com>
2020-06-02 23:12:45 +0800

    first commit

--- springcloudconfig/microservice-config-client-2/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
+++ springcloudconfig/microservice-config-client-2/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
@@ -1,27 +1,18 @@
 package com.itmuch.cloud.study.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigClientController {
-//  @Value("${profile}")
-//  private String profile;
-//
-//  @GetMapping("/profile")
-//  public String hello() {
-//    return this.profile;
-//  }
 
-  @Autowired
-  private Environment env;
+  @Value("${from}")
+  private String from;
 
   @GetMapping("/from")
   public String hello() {
-    return env.getProperty("from","undefine");
+    return this.from;
   }
 
 }
