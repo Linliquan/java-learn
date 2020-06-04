43a47da128e4a157d4a7866b38734bd92ff3ad0d
linliquan <1511387837@qq.com>
2020-06-04 23:22:18 +0800

    first commit

---
+++ springcloudconfig/microservice-config-client-refresh/src/main/java/com/itmuch/cloud/study/ConfigClientRefreshApplication.java
@@ -0,0 +1,11 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRefreshApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientRefreshApplication.class, args);
+  }
+}
