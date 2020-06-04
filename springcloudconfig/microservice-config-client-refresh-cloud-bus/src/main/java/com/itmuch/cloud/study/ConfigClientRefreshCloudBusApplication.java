c13ef6c1b86f535e7aaa36347e773995b1c7fe87
linliquan <1511387837@qq.com>
2020-06-04 23:24:53 +0800

    first commit

---
+++ springcloudconfig/microservice-config-client-refresh-cloud-bus/src/main/java/com/itmuch/cloud/study/ConfigClientRefreshCloudBusApplication.java
@@ -0,0 +1,11 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRefreshCloudBusApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientRefreshCloudBusApplication.class, args);
+  }
+}
