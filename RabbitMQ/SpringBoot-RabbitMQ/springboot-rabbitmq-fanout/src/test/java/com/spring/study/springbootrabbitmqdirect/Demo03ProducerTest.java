eb1f03d4e9aa238c4e3324be93eb76eec3fcfdd9
linliquan <1511387837@qq.com>
2020-06-08 13:44:33 +0800

    first commit

---
+++ RabbitMQ/SpringBoot-RabbitMQ/springboot-rabbitmq-fanout/src/test/java/com/spring/study/springbootrabbitmqdirect/Demo03ProducerTest.java
@@ -0,0 +1,39 @@
+package com.spring.study.springbootrabbitmqdirect;
+
+import com.spring.study.springbootrabbitmqdirect.producer.Demo03Producer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-07 11:01
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo03ProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Demo03Producer producer;
+
+    @Test
+    public void testSyncSend() throws InterruptedException {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        producer.syncSend(id);
+        logger.info("[testSyncSend][发送编号：[{}] 发送成功]", id);
+
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+    }
+
+}
