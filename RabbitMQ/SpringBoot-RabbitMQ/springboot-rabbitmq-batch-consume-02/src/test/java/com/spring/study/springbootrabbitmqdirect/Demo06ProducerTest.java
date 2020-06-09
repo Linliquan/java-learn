2bc56945597b375b886f6dc6a7c8d528d977db74
linliquan <1511387837@qq.com>
2020-06-09 14:53:52 +0800

    first commit

---
+++ RabbitMQ/SpringBoot-RabbitMQ/springboot-rabbitmq-batch-consume-02/src/test/java/com/spring/study/springbootrabbitmqdirect/Demo06ProducerTest.java
@@ -0,0 +1,54 @@
+package com.spring.study.springbootrabbitmqdirect;
+
+
+import com.spring.study.springbootrabbitmqdirect.producer.Demo06Producer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-07 14:30
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo06ProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Demo06Producer producer;
+
+    @Test
+    public void testSyncSend01() throws InterruptedException {
+        // 发送 3 条消息
+        this.testSyncSend(3);
+    }
+
+    @Test
+    public void testSyncSen02() throws InterruptedException {
+        // 发送 10 条消息
+        this.testSyncSend(11);
+    }
+
+    private void testSyncSend(int n) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            // 同步发送消息
+            int id = (int) (System.currentTimeMillis() / 1000);
+            producer.syncSend(id);
+            logger.info("[testSyncSendMore][发送编号：[{}] 发送成功]", id);
+        }
+
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+    }
+
+}
