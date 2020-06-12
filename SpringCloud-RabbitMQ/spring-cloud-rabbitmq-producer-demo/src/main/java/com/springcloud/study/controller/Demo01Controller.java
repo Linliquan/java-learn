feb93733ec5b8c5210540dca9860736fd0f89bf6
linliquan <1511387837@qq.com>
2020-06-12 23:36:06 +0800

    first commit

---
+++ SpringCloud-RabbitMQ/spring-cloud-rabbitmq-producer-demo/src/main/java/com/springcloud/study/controller/Demo01Controller.java
@@ -0,0 +1,11 @@
+package com.springcloud.study.controller;
+
+/**
+ * @program: java-learn
+ * @description: 创建 Demo01Controller 类，提供发送消息的 HTTP 接口
+ * @author: liquan
+ * @create: 2020-06-12 23:34
+ **/
+public class Demo01Controller {
+
+}
