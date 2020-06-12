41cc38772df2e350822a3d0e04d4ebe8d313e4f2
linliquan <1511387837@qq.com>
2020-06-12 23:36:13 +0800

    first commit

---
+++ SpringCloud-RabbitMQ/spring-cloud-rabbitmq-producer-demo/src/main/java/com/springcloud/study/RabbitmqProducerDemoApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-12 23:27
+ **/
+@SpringBootApplication
+public class RabbitmqProducerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducerDemoApplication.class, args);
+    }
+}
