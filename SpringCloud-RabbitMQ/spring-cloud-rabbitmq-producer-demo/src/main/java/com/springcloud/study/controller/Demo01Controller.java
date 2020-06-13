5569d7f4f602f4a21d2171e03f7efe9a57c3bf9a
linliquan <1511387837@qq.com>
2020-06-13 12:17:00 +0800

    first commit

--- SpringCloud-RabbitMQ/spring-cloud-rabbitmq-producer-demo/src/main/java/com/springcloud/study/controller/Demo01Controller.java
+++ SpringCloud-RabbitMQ/spring-cloud-rabbitmq-producer-demo/src/main/java/com/springcloud/study/controller/Demo01Controller.java
@@ -1,11 +1,45 @@
 package com.springcloud.study.controller;
 
+import com.springcloud.study.message.Demo01Message;
+import com.springcloud.study.message.MySource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
 /**
  * @program: java-learn
  * @description: 创建 Demo01Controller 类，提供发送消息的 HTTP 接口
  * @author: liquan
  * @create: 2020-06-12 23:34
  **/
+@RestController
+@RequestMapping("/demo01")
 public class Demo01Controller {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MySource mySource;
+
+    @GetMapping("/send")
+    public boolean send() {
+        // <1> 创建 Message
+        Demo01Message message = new Demo01Message()
+                .setId(new Random().nextInt());
+        // <2> 创建 Spring Message 对象
+        Message<Demo01Message> springMessage = MessageBuilder.withPayload(message)
+                .build();
+        // <3> 发送消息
+        boolean result = mySource.demo01Output().send(springMessage);
+        logger.info("[send][发送编号：[{}] 发送成功]", message.getId());
+        return result;
+    }
 
 }
