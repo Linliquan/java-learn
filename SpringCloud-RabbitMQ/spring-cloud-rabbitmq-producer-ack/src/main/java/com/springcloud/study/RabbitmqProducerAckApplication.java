9643f31f580ea1898ee94fc01cacb358a08c1086
linliquan <1511387837@qq.com>
2020-06-14 21:47:47 +0800

    first commit

---
+++ SpringCloud-RabbitMQ/spring-cloud-rabbitmq-producer-ack/src/main/java/com/springcloud/study/RabbitmqProducerAckApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.study;
+
+import com.springcloud.study.message.MySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-12 23:27
+ **/
+@SpringBootApplication
+@EnableBinding(MySource.class)
+public class RabbitmqProducerAckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducerAckApplication.class, args);
+    }
+}
