3abe68017bbb1b3834cd31fc0efa1052130b7686
linliquan <1511387837@qq.com>
2020-06-15 01:18:39 +0800

    first commit

---
+++ SpringCloud/spring-cloud-study/microservice-simple-provider-user-sleuth-hoxton-sr5/src/main/java/com/itmuch/cloud/study/entity/User.java
@@ -0,0 +1,33 @@
+package com.itmuch.cloud.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-15 01:18
+ **/
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String username;
+    @Column
+    private String name;
+    @Column
+    private Integer age;
+    @Column
+    private BigDecimal balance;
+}
+
