3a57358819b1fa7be842e3b81eb9cf5edccb76a4
linliquan <1511387837@qq.com>
2020-06-16 21:13:44 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-quick-start-demo/src/main/java/com/rocketmq/study/QuickStartApplication.java
@@ -0,0 +1,17 @@
+package com.rocketmq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-learn
+ * @description: rocketmq发送消息的三种方式：同步发送、异步发送、直接发送
+ * @author: liquan
+ * @create: 2020-06-16 12:26
+ **/
+@SpringBootApplication
+public class QuickStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class, args);
+    }
+}
