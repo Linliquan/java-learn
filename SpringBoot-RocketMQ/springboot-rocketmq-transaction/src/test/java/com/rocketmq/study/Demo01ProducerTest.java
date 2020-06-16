bd8b0303f59d68789ce96f4bd39562f857f77486
linliquan <1511387837@qq.com>
2020-06-16 18:39:41 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-rocketmq-transaction/src/test/java/com/rocketmq/study/Demo01ProducerTest.java
@@ -0,0 +1,39 @@
+package com.rocketmq.study;
+
+import com.rocketmq.study.producer.Demo07Producer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-16 13:17
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RocketMqTransactionApplication.class)
+public class Demo01ProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private Demo07Producer producer;
+
+    @Test
+    public void testSendMessageInTransaction() throws InterruptedException {
+        int id = (int) (System.currentTimeMillis() / 1000);
+        SendResult result = producer.sendMessageInTransaction(id);
+        logger.info("[testSendMessageInTransaction][发送编号：[{}] 发送结果：[{}]]", id, result);
+
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+    }
+}
