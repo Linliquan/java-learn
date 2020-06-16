10e37ab23825c5a4df456c9ef40e9bd60a4166e2
linliquan <linliquan@smodock.com>
2020-06-16 13:30:56 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-quick-start-demo/src/main/java/com/rocketmq/study/consumer/Demo01AConsumer.java
@@ -0,0 +1,31 @@
+package com.rocketmq.study.consumer;
+
+import com.rocketmq.study.message.Demo01Message;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-16 12:46
+ **/
+@Component
+@RocketMQMessageListener(
+        topic = Demo01Message.TOPIC,
+        consumerGroup = "demo01-A-consumer-group-" + Demo01Message.TOPIC
+)
+public class Demo01AConsumer implements RocketMQListener<MessageExt> {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onMessage(MessageExt message) {
+        logger.info("[onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+    }
+
+}
