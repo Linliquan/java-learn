23d4a9db1a04a0d1d0c8edd44c0dc0aef81d1da2
linliquan <1511387837@qq.com>
2020-06-17 23:50:21 +0800

    first commit

---
+++ SpringCloud-RocketMQ/spring-cloud-rocketmq-producer-actuator/src/main/java/com/rocketmq/study/message/MySource.java
@@ -0,0 +1,11 @@
+package com.rocketmq.study.message;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface MySource {
+
+    @Output("demo01-output")
+    MessageChannel demo01Output();
+
+}
