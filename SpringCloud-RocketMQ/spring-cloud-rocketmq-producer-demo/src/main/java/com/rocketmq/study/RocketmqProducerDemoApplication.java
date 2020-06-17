1c10d001c825b47c413e5cc512eebafe5898a16f
linliquan <1511387837@qq.com>
2020-06-17 17:15:27 +0800

    first commit

---
+++ SpringCloud-RocketMQ/spring-cloud-rocketmq-producer-demo/src/main/java/com/rocketmq/study/RocketmqProducerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.rocketmq.study;
+
+import com.rocketmq.study.message.MySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-06-16 21:29
+ **/
+@SpringBootApplication
+@EnableBinding(MySource.class)
+public class RocketmqProducerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerDemoApplication.class, args);
+    }
+}
