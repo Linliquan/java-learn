dcddd82a2dd7533258a35d50f6fba8e7b637bafb
linliquan <1511387837@qq.com>
2020-06-18 00:46:57 +0800

    first commit

---
+++ SpringCloud-RocketMQ/spring-cloud-rocketmq-consumer-orderly/src/main/java/com/rocketmq/study/RocketmqConsumerOrderlyApplication.java
@@ -0,0 +1,20 @@
+package com.rocketmq.study;
+
+import com.rocketmq.study.listener.MySink;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+/**
+ * @program: java-learn
+ * @description: 
+ * @author: liquan
+ * @create: 2020-06-16 21:29
+ **/
+@SpringBootApplication
+@EnableBinding(MySink.class)
+public class RocketmqConsumerOrderlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqConsumerOrderlyApplication.class, args);
+    }
+}
