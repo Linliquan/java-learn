3972e46aaf5e481afe70e88d1d27588e3fe6e0ef
linliquan <1511387837@qq.com>
2020-06-28 15:08:18 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-rocketmq-batch/src/main/java/com/rocketmq/study/RocketMqBatchApplication.java
@@ -0,0 +1,17 @@
+package com.rocketmq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-learn
+ * @description: 批量发送消息
+ * @author: liquan
+ * @create: 2020-06-16 12:26
+ **/
+@SpringBootApplication
+public class RocketMqBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqBatchApplication.class, args);
+    }
+}
