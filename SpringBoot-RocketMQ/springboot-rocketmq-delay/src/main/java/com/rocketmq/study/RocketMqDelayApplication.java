efac0b3870e7f891eb643c237373219245a2ebb4
linliquan <1511387837@qq.com>
2020-07-04 14:13:54 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-rocketmq-delay/src/main/java/com/rocketmq/study/RocketMqDelayApplication.java
@@ -0,0 +1,17 @@
+package com.rocketmq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-learn
+ * @description: 定时消息
+ * @author: liquan
+ * @create: 2020-06-16 12:26
+ **/
+@SpringBootApplication
+public class RocketMqDelayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqDelayApplication.class, args);
+    }
+}
