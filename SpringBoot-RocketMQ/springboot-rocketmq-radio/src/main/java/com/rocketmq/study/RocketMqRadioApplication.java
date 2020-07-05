8af3d6dba1fc8275f4964e52645d15fb8bd71bdf
linliquan <1511387837@qq.com>
2020-07-05 20:16:20 +0800

    first commit

---
+++ SpringBoot-RocketMQ/springboot-rocketmq-radio/src/main/java/com/rocketmq/study/RocketMqRadioApplication.java
@@ -0,0 +1,17 @@
+package com.rocketmq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java-learn
+ * @description: 广播消费
+ * @author: liquan
+ * @create: 2020-06-16 12:26
+ **/
+@SpringBootApplication
+public class RocketMqRadioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqRadioApplication.class, args);
+    }
+}
