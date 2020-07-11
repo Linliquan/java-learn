a245a2c84e60caf950385338d8018873d69677d0
linliquan <1511387837@qq.com>
2020-07-11 22:38:25 +0800

    first commit

---
+++ SpringCloudAlibaba/microservice-config-server/src/main/java/com/springcloud/server/config/controller/ConfigClientController.java
@@ -0,0 +1,16 @@
+package com.springcloud.server.config.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: java-learn
+ * @description:
+ * @author: liquan
+ * @create: 2020-07-11 21:12
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+}
