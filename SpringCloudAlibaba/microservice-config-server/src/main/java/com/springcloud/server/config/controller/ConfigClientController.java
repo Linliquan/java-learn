4b3d01068b7beab5984225c23ea3177296aefe34
linliquan <1511387837@qq.com>
2020-07-11 22:39:22 +0800

    first commit

--- SpringCloudAlibaba/microservice-config-server/src/main/java/com/springcloud/server/config/controller/ConfigClientController.java
+++ SpringCloudAlibaba/microservice-config-server/src/main/java/com/springcloud/server/config/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.springcloud.server.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +18,9 @@ public class ConfigClientController {
     @Value("${profile}")
     private String profile;
 
+    @GetMapping("/profile")
+    public String hello() {
+        return this.profile;
+    }
 
 }
