c1bd77adf6b2b669f568d6a774e3b6e26e515d00
linliquan <1511387837@qq.com>
2020-07-23 22:38:32 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/clientobject/MiscMetricCO.java
@@ -0,0 +1,37 @@
+package com.springcloud.study.colaspringcloud.dto.clientobject;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * MiscMetricCO
+ *
+ * @author Frank Zhang
+ * @date 2019-03-04 10:58 AM
+ */
+@Data
+public class MiscMetricCO extends AbstractMetricCO {
+
+    /**
+     * 名称
+     */
+    @NotEmpty
+    private String name;
+
+    /**
+     * 内容
+     */
+    @NotEmpty
+    private String content;
+
+    /**
+     * 文档链接
+     */
+    private String docUrl;
+
+    /**
+     * 代码链接
+     */
+    private String codeUrl;
+}
