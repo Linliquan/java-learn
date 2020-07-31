a8e520e463188559e9362ed70b5414bd56b502dc
linliquan <1511387837@qq.com>
2020-07-31 23:58:35 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/PaperMetricAddCmd.java
@@ -0,0 +1,18 @@
+package com.springcloud.study.colaspringcloud.dto;
+
+import com.springcloud.study.colaspringcloud.dto.clientobject.PaperMetricCO;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * PaperMetricAddCmd
+ *
+ * @author Frank Zhang
+ * @date 2019-03-03 11:38 AM
+ */
+@Data
+public class PaperMetricAddCmd extends CommonCommand{
+    @NotNull
+    private PaperMetricCO paperMetricCO;
+}
