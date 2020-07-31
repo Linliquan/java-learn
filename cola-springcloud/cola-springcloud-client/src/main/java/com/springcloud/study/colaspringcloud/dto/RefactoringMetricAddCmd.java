2f4af014d60c80b62bed41d9f5581f8965b8d383
linliquan <1511387837@qq.com>
2020-07-31 23:58:55 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/RefactoringMetricAddCmd.java
@@ -0,0 +1,18 @@
+package com.springcloud.study.colaspringcloud.dto;
+
+import com.springcloud.study.colaspringcloud.dto.clientobject.RefactoringMetricCO;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * RefactoringMetricAddCmd
+ *
+ * @author Frank Zhang
+ * @date 2019-03-04 11:04 AM
+ */
+@Data
+public class RefactoringMetricAddCmd extends CommonCommand{
+    @NotNull
+    private RefactoringMetricCO refactoringMetricCO;
+}
