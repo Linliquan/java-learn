ec319cba4469ad205f43a740f9b6bd66a290809e
linliquan <1511387837@qq.com>
2020-07-31 23:58:16 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/MetricDeleteCmd.java
@@ -0,0 +1,17 @@
+package com.springcloud.study.colaspringcloud.dto;
+
+import lombok.Data;
+
+/**
+ * MetricDeleteCmd
+ *
+ * @author Frank Zhang
+ * @date 2019-03-01 10:11 AM
+ */
+@Data
+public class MetricDeleteCmd extends CommonCommand{
+    /**
+     * Metric ID
+     */
+    private String metricId;
+}
