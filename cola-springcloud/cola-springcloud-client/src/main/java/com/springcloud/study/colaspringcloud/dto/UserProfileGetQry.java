29f5b296dda39d58b80d50b3eb93b61b229ad97a
linliquan <1511387837@qq.com>
2020-07-31 23:59:29 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/UserProfileGetQry.java
@@ -0,0 +1,14 @@
+package com.springcloud.study.colaspringcloud.dto;
+
+import lombok.Data;
+
+@Data
+public class UserProfileGetQry extends CommonCommand {
+    private String userId;
+    private String id;
+
+    public UserProfileGetQry(){
+
+    }
+
+}
