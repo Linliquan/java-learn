38c7d1c5d4da8442f7283adfb7f70f883aa5445a
linliquan <1511387837@qq.com>
2020-07-31 23:59:10 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-client/src/main/java/com/springcloud/study/colaspringcloud/dto/SharingMetricAddCmd.java
@@ -0,0 +1,18 @@
+package com.springcloud.study.colaspringcloud.dto;
+
+import com.springcloud.study.colaspringcloud.dto.clientobject.SharingMetricCO;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * SharingMetricAddCmd
+ *
+ * @author Frank Zhang
+ * @date 2019-03-01 10:12 AM
+ */
+@Data
+public class SharingMetricAddCmd extends CommonCommand{
+    @NotNull
+    private SharingMetricCO sharingMetricCO;
+}
