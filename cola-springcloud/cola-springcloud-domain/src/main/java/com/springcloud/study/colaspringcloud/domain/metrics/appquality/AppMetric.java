096be5c53c90684016c0d8c84b47c58774ac7e36
linliquan <1511387837@qq.com>
2020-08-02 17:48:54 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-domain/src/main/java/com/springcloud/study/colaspringcloud/domain/metrics/appquality/AppMetric.java
@@ -0,0 +1,26 @@
+package com.springcloud.study.colaspringcloud.domain.metrics.appquality;
+
+import com.springcloud.study.colaspringcloud.domain.metrics.SubMetric;
+import com.springcloud.study.colaspringcloud.domain.metrics.SubMetricType;
+
+public class AppMetric extends SubMetric {
+
+    public AppMetric(){
+        this.subMetricType = SubMetricType.App;
+    }
+
+    @Override
+    public double getWeight() {
+        return metricOwner.getWeight().getUnanimousWeight();
+    }
+
+    @Override
+    public double calculateScore() {
+        int appCount = super.getMetricItemList().size();
+        if (appCount == 0){
+            return 0;
+        }
+        double sumScore = super.calculateScore();
+        return sumScore/appCount;
+    }
+}
