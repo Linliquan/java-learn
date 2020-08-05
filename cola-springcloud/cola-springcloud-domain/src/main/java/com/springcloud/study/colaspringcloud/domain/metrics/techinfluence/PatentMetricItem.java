fa2b91863e565371f55c0ab71d30934ca3e8c1fd
linliquan <1511387837@qq.com>
2020-08-05 22:49:47 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-domain/src/main/java/com/springcloud/study/colaspringcloud/domain/metrics/techinfluence/PatentMetricItem.java
@@ -0,0 +1,41 @@
+package com.springcloud.study.colaspringcloud.domain.metrics.techinfluence;
+
+import com.alibaba.fastjson.JSON;
+import com.springcloud.study.colaspringcloud.domain.metrics.MetricItem;
+import lombok.Data;
+
+/**
+ * 技术创新指标项
+ * @author xueliang.sxl
+ */
+@Data
+public class PatentMetricItem extends MetricItem {
+
+    private String patentName;
+    private String patentDesc;
+    private String patentNo;
+    private String patentUrl;
+    private AuthorType authorType;
+
+    public PatentMetricItem(){
+
+    }
+
+    public PatentMetricItem(String patentName, String patentDesc, String patentNo, String patentUrl, AuthorType authorType){
+        this.patentName = patentName;
+        this.patentDesc = patentDesc;
+        this.patentNo = patentNo;
+        this.patentUrl = patentUrl;
+        this.authorType = authorType;
+    }
+
+    public static PatentMetricItem valueOf(String json){
+        return JSON.parseObject(json, PatentMetricItem.class);
+    }
+
+    @Override
+    public double calculateScore() {
+       return authorType.getScore();
+    }
+
+}
