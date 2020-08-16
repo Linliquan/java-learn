d11b7f9a5684286fbe289daca4932bbd7b05dce9
linliquan <1511387837@qq.com>
2020-08-16 14:38:52 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-domain/src/main/java/com/springcloud/study/colaspringcloud/domain/metrics/MetricItem.java
@@ -0,0 +1,48 @@
+package com.springcloud.study.colaspringcloud.domain.metrics;
+
+import com.alibaba.cola.domain.EntityObject;
+import com.alibaba.cola.logger.Logger;
+import com.alibaba.cola.logger.LoggerFactory;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.springcloud.study.colaspringcloud.domain.user.UserProfile;
+import lombok.Data;
+
+/**
+ * MetricItem
+ * 指标项，多个指标项可以构成一个指标
+ * @author Frank Zhang
+ * @date 2018-07-04 1:23 PM
+ */
+@Data
+public abstract class MetricItem extends EntityObject implements Measurable{
+
+    private static Logger logger = LoggerFactory.getLogger(MetricItem.class);
+
+    /**
+     * The metric this MetricItem belongs to
+     */
+    @JSONField(serialize = false)
+    private SubMetric subMetric;
+
+    /**
+     * The owner of this MetricItem
+     */
+    @JSONField(serialize = false)
+    private UserProfile metricOwner;
+
+    public void setSubMetric(SubMetric subMetric){
+        this.subMetric = subMetric;
+        this.metricOwner = subMetric.getMetricOwner();
+    }
+    /**
+     * 将度量项的转成JSON
+     * @return
+     */
+    public String toJsonString() {
+        String jsonStr = JSON.toJSONString(this, JSONPropertyFilter.singleton);
+        logger.debug("\n From : " + this + " \n To: " + jsonStr);
+        return jsonStr;
+    }
+
+}
