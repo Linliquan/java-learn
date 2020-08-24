ff08ed31d7d2d8727ad18e556df4ab2dc6f5f48a
linliquan <1511387837@qq.com>
2020-08-24 10:04:14 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-infrastructure/src/main/java/com/springcloud/study/colaspringcloud/config/ColaConfig.java
@@ -0,0 +1,21 @@
+package com.springcloud.study.colaspringcloud.config;
+
+import com.alibaba.cola.boot.SpringBootstrap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for COLA framework
+ */
+@Configuration
+@ComponentScan(value = {"com.alibaba.cola","ColaSpringcloudApplication"})
+public class ColaConfig {
+
+    @Bean(initMethod = "init")
+    public SpringBootstrap bootstrap() {
+        SpringBootstrap bootstrap = new SpringBootstrap();
+        return bootstrap;
+    }
+
+}
