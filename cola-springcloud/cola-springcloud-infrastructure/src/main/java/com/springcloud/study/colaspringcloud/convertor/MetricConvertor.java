dcd4d9d1c80ac529831e860815a8806361354c59
linliquan <1511387837@qq.com>
2020-08-30 21:32:31 +0800

    first commit

---
+++ cola-springcloud/cola-springcloud-infrastructure/src/main/java/com/springcloud/study/colaspringcloud/convertor/MetricConvertor.java
@@ -0,0 +1,21 @@
+package com.springcloud.study.colaspringcloud.convertor;
+
+
+import com.springcloud.study.colaspringcloud.domain.metrics.MetricItem;
+import com.springcloud.study.colaspringcloud.gatewayimpl.database.dataobject.MetricDO;
+
+/**
+ * @author frankzhang
+ */
+public class MetricConvertor{
+
+    public static MetricDO toDataObject(MetricItem metricItem){
+        MetricDO metricDO = new MetricDO();
+        metricDO.setUserId(metricItem.getMetricOwner().getUserId());
+        metricDO.setMainMetric(metricItem.getSubMetric().getParent().getCode());
+        metricDO.setSubMetric(metricItem.getSubMetric().getCode());
+        metricDO.setMetricItem(metricItem.toJsonString());
+        return metricDO;
+    }
+
+}
