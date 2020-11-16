abd106b3a905e720e4f73211e0b2bc3af82ae177
linliquan <1511387837@qq.com>
2020-11-16 20:45:38 +0800

    无参数有返回值

--- Java8流式编程/src/Lambda/test/Test1.java
+++ Java8流式编程/src/Lambda/test/Test1.java
@@ -1,8 +1,6 @@
 package Lambda.test;
 
-import Lambda.interfaces.NoReturnMultiParam;
-import Lambda.interfaces.NoReturnNoParam;
-import Lambda.interfaces.NoReturnOneParam;
+import Lambda.interfaces.*;
 
 /**
  * @author linliquan
@@ -19,19 +17,40 @@ public class Test1 {
         noReturnNoParam.method();
 
         // 2.一个参数无返回
-        NoReturnOneParam noReturnOneParam = (int a) -> {
+        NoReturnOneParam noReturnOneParam = (a) -> {
             System.out.println("一个参数无返回 a =" + a*2);
         };
         noReturnOneParam.method(6);
 
         // 3.多个参数无返回
-        NoReturnMultiParam noReturnMultiParam = (int a,int b) -> {
+        NoReturnMultiParam noReturnMultiParam = (a, b) -> {
             System.out.println("多个参数无返回值 " + (a + b));
         };
         noReturnMultiParam.method(3,4);
 
+        // 4.无参数有返回值
+        ReturnNoParam returnNoParam = () -> {
+            System.out.println("无参数有返回值");
+            return 5;
+        };
+        int b = returnNoParam.method();
+        System.out.println("b = "+b);
 
+        // 5.一个参数有返回值
+        ReturnOneParam returnOneParam = (a) -> {
+            System.out.println("一个参数有返回值");
+            return a + 5;
+        };
+        int a = returnOneParam.method(6);
+        System.out.println("a = " + a);
 
+        // 6.多个参数有返回值
+        ReturnMultiParam returnMultiParam = (c, d) -> {
+            System.out.println("c - d = " + (c - d));
+            return c - d;
+        };
+        int result = returnMultiParam.method(8, 2);
+        System.out.println("result = " + result);
 
 
     }
