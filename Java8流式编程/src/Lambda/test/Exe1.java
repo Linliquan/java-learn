cd5564c6e8e22d493de383b7b1f94c5d6e0f03f0
linliquan <1511387837@qq.com>
2020-12-03 21:40:38 +0800

    first commit

--- Java8流式编程/src/Lambda/test/Exe1.java
+++ Java8流式编程/src/Lambda/test/Exe1.java
@@ -26,6 +26,7 @@ public class Exe1 {
         // lambda2 引用了已经实现的 doubleNum 方法
         ReturnOneParam lambda2 = Exe1::doubleNum;
         System.out.println(lambda2.method(3));
+        System.out.println(lambda2.method(3));
 
         // 普通方法为对象调用，静态方法为类名调用
         Exe1 exe1 = new Exe1();
