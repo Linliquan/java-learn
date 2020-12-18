29e5e0f5520fb4fbbc12215483cd8c9cfcef68da
linliquan <1511387837@qq.com>
2020-12-18 09:39:57 +0800

    构造方法的引用

--- Java8流式编程/src/Lambda/test/Exe2.java
+++ Java8流式编程/src/Lambda/test/Exe2.java
@@ -5,12 +5,20 @@ import Lambda.interfaces.ItemCreatorParamContruct;
 
 /**
  * @author linliquan
- * @description:
+ * @description: 构造方法的引用
  * @create 2020/12/17 17:38
  */
 public class Exe2 {
     public static void main(String[] args) {
         ItemCreatorBlankConstruct creator = () -> new Item();
+        Item item = creator.getItem();
+
+        ItemCreatorBlankConstruct creator2  = Item::new;
+        Item item1 = creator2.getItem();
+        System.out.println(item1);
+
+//        ItemCreatorParamContruct creator3 = Item::new;
+//        Item item3 = creator3.getItem(112, "鼠标", 135.99);
 
     }
 }
