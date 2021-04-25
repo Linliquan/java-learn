f4fd59dcb9884b31fa667fd5a550bb75028ef533
linliquan <1511387837@qq.com>
2021-04-25 17:27:36 +0800

    添加MD5加密工具

---
+++ springboot-common/src/test/java/com/spring/study/springbootcommon/SpringbootCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.study.springbootcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
