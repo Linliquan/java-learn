e7752905e8dd49372cb3c9bb1b19de12341e5896
linliquan <1511387837@qq.com>
2021-06-04 17:35:21 +0800

    first commit

---
+++ 集合/src/TreeSet/TestNum.java
@@ -0,0 +1,14 @@
+package TreeSet;
+
+
+/**
+ * @author linliquan
+ * @description:
+ * @create 2021/6/4 17:29
+ */
+public class TestNum {
+    public static void main(String[] args) {
+        int a = 3_2;
+        System.out.println(a);
+    }
+}
