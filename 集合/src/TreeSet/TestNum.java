b3b272dfe7baba862a1d57171253cb8309f21b13
linliquan <1511387837@qq.com>
2021-06-07 14:25:01 +0800

    first commit

--- 集合/src/TreeSet/TestNum.java
+++ 集合/src/TreeSet/TestNum.java
@@ -9,6 +9,8 @@ package TreeSet;
 public class TestNum {
     public static void main(String[] args) {
         int a = 3_2;
+        int b = 3_2 + 1;
         System.out.println(a);
+        System.out.println(b);
     }
 }
